50bee171f22a2b1b98ecb754a074182170ccb404
rorrocatalam <rodrigo.catalan.b@ug.uchile.cl>
2024-06-03 22:48:58 -0400

    Agrego definiciones de clases iniciales a modelo.py

--- modelo.py
+++ modelo.py
@@ -1,3 +1,63 @@
-"""
-Aquí irán las clases y sus métodos que harán funcionar el sistema
-"""
+#=============================================================================
+# Estructuras basicas
+
+class Hecho:
+    def __init__(self, prem, vc):
+        self.prem   = prem
+        self.vc     = vc
+
+class Regla:
+    def __init__(self, prem, con, vc):
+        self.prem   = prem
+        self.con    = con
+        self.vc     = vc
+    
+    def es_evaluable(self, bdh):
+        """
+        Metodo para saber si la regla es evaluable en la base de hechos
+        """
+        # Tripletas de la base de hechos
+        bdh_lista_trip = bdh.lista_trip
+        # Retorno booleano si la regla se puede evaluar
+        return set(self.prem).issubset(bdh_lista_trip)
+
+#=============================================================================
+# Conjuntos a utilizar
+
+class BaseDeHechos:
+    def __init__(self):
+        self.lista_trip  = []
+        self.lista_vc    = []
+    
+    def agregar_hecho(self, hecho):
+        """
+        Metodo para agregar hechos
+        """
+        # Premisa y valor del hecho
+        prem = hecho.prem
+        vc = hecho.vc
+        # Guardar valores
+        self.lista_trip.append(prem[0])
+        self.lista_vc.append(vc)
+
+class BaseDeReglas:
+    def __init__(self, reglas):
+        self.reglas = reglas
+
+    def trip_en_con(self, trip):
+        """
+        Metodo para encontrar reglas que tienen como conclusion una determinada tripleta
+        """
+        # Lista de reglas que cumplen requisito
+        lista_reglas = []
+
+        # Se revisan todas las reglas presentes
+        for regla in self.reglas:
+            # Conclusiones de la regla
+            regla_con = regla.con
+            # Se guarda la regla si cumple requisito
+            if trip in regla_con:
+                lista_reglas.append(regla)
+        
+        # Retorno lista de reglas 
+        return lista_reglas
